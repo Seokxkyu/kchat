b634c4be5f14cc1a4ba86f94763122f7169ec053
Seokxkyu <kyuseok000@gmail.com>
2024-08-21 14:19:54 +0900

    time change

--- src/kchat/kafka/con.py
+++ src/kchat/kafka/con.py
@@ -5,7 +5,7 @@ consumer = KafkaConsumer(
         'topic1',
         bootstrap_servers = ['localhost:9092'],
         value_deserializer=lambda x: loads(x.decode('utf-8')),
-        consumer_timeout_ms=5000
+        consumer_timeout_ms=15000
         )
 
 print('[Start] get consumer')
