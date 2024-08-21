4a7412b8d480449ac80d89fa79bde56e2ab73d19
Seokxkyu <kyuseok000@gmail.com>
2024-08-21 17:37:10 +0900

    pchat functions

--- src/kchat/kafka/pchat.py
+++ src/kchat/kafka/pchat.py
@@ -1,9 +1,12 @@
 from kafka import KafkaProducer
 from json import dumps
 import time
+import json
 
 producer = KafkaProducer(
         # TODO
+        bootstrap_servers=['localhost:9092'],
+        value_serializer=lambda x:json.dumps(x).encode('utf-8')
 )
 
 print("채팅 프로그램 - 메시지 발신자")
@@ -16,6 +19,7 @@ while True:
 
     data = {'message': msg, 'time': time.time()}
     # TODO
-    producer
+    producer.send('chat', value=data)
+    producer.flush()
 
 print("채팅 종료")
